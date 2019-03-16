3b2fc7c5e29914b0a45f87678b6a37d66414023a
Adam Kearney <aklives@hotmail.com>
2019-03-16 03:14:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    last_element = array.slice!(-1)
+    sentence = array.join(", ")
+    sentence << ", and #{last_element}"
+  end
 end
